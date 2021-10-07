5b02885db56748f393f0e6830d716525b2f11196
lyubakek <kovalchuk.lyubaa@gmail.com>
2021-10-07 17:36:46 +0300

    Initial Commit

---
+++ Scraper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scraper
+//
+//  Created by Liubov Kovalchuk on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
