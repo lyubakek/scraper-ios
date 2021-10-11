e00e2cc216c1e39621d2f24334934a46a61016b3
lyubakek <kovalchuk.lyubaa@gmail.com>
2021-10-11 23:17:41 +0300

    implement some methods which get html string of page and find all url on this page

---
+++ Scraper/ResultTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ResultTableViewCell.swift
+//  Scraper
+//
+//  Created by Liubov Kovalchuk on 08.10.2021.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var urlLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Scraper/ViewController.swift
+++ Scraper/ViewController.swift
@@ -11,6 +11,9 @@ import CoreData
 
 @objcMembers
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+//    ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=
+
+    var allowedSymbols = [ "A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", ".", "_", "~", ":", "/", "?", "#", "[", "]", "@", "!", "$", "&", "(", ")", "*", "+", ",", ";", "=", "'"]
 
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
@@ -23,13 +26,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var tableView: UITableView!
     
+    var startUrl: URL!
+    
+    var htmlString: String = " "
+    
     let cellReuseIdentifier = "cell"
     
-    var urlArrayTest: [String] = ["https://example.com", "https://example.com/1", "https://example.com/2", "https://example.com/3", "https://example.com/4"]
+    var urlArrayTest: [String] = ["https://example.com", "https://example.com/1", "https://example.com/2", "https://example.com/3", "https://example.com/4https://example.com/4"]
     
     var baseUrl: URL!
-
-    
+    var threadCount: Int!
+    var textResult: String!
+    var maxUrlCount: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +48,43 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         textResultTextField.delegate = self
         maxUrlCoutTextField.delegate = self
         
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+//        tableView.register(ResultTableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getDataFromUrl()
+        findUrlInString()
+    }
+    
+    func getDataFromUrl() {
+        let myURLString = "https://cinemaciti.ua/ua/cinema-city-kyiv/mv/bond_25/"
+        guard let myURL = URL(string: myURLString) else {
+            print("Error: \(myURLString) doesn't seem to be a valid URL")
+            return
+        }
+
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            htmlString = myHTMLString
+            
+            print("HTML : \(myHTMLString)")
+        } catch let error {
+            print("Error: \(error)")
+        }
+    }
+    
+    func findUrlInString() {
+        let input = htmlString
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        let matches = detector.matches(in: input, options: [], range: NSRange(location: 0, length: input.utf16.count))
+
+        for match in matches {
+            guard let range = Range(match.range, in: input) else { continue }
+            let url = input[range]
+            print("url will be on next row")
+            print(url)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -51,9 +92,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! ResultTableViewCell
         
-        cell.textLabel?.text = self.urlArrayTest[indexPath.row]
+        cell.urlLabel.text = self.urlArrayTest[indexPath.row]
+        cell.statusLabel.text = "OK"
         
         return cell
         
@@ -67,10 +109,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        var newText = textField.text! as NSString
-        newText = newText.replacingCharacters(in: range, with: string) as NSString
-        
+        if textField == startUrlTextField {
+            var newTextUrl = textField.text! as NSString
+            newTextUrl = newTextUrl.replacingCharacters(in: range, with: string) as NSString
+//            startUrl = URL(string: newTextUrl as String)
+            print(newTextUrl)
+        } else if textField == threadCountTextField {
+            var threads = textField.text! as NSString
+            threads = threads.replacingCharacters(in: range, with: string) as NSString
+            print(threads)
+        } else if textField == textResultTextField {
+            var findText = textField.text! as NSString
+            findText = findText.replacingCharacters(in: range, with: string) as NSString
+            print(findText)
+        } else if textField == maxUrlCoutTextField {
+            var urlCount = textField.text! as NSString
+            urlCount = urlCount.replacingCharacters(in: range, with: string) as NSString
+            print(urlCount)
+        }
+
         return true
+    }
+    
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == startUrlTextField {
+            startUrl = URL(string: textField.text!)
+            print("this is a start url " + "\(startUrl as Any)")
+        } else if textField == threadCountTextField {
+            threadCount = Int(textField.text!)
+            print("this is how many threads can be " + "\(threadCount as Any)")
+        } else if textField == textResultTextField {
+            textResult = String(textField.text!)
+            print("this is text to find " + "\(textResult as Any)")
+        } else if textField == maxUrlCoutTextField {
+            maxUrlCount = Int(textField.text!)
+            print("this is url count when we can stop " + "\(maxUrlCount as Any)")
+        }
+        
     }
 
     func startButtonTapped() {
