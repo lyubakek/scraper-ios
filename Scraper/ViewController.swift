cd9becc4fb1a5cd07b98477501d7bf341bd10050
lyubakek <kovalchuk.lyubaa@gmail.com>
2021-10-13 00:05:34 +0300

    create set, add touch up inside actions to buttons,

--- Scraper/ViewController.swift
+++ Scraper/ViewController.swift
@@ -4,6 +4,9 @@
 //
 //  Created by Liubov Kovalchuk on 07.10.2021.
 //
+//    ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=
+//    var allowedSymbols = [ "A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", ".", "_", "~", ":", "/", "?", "#", "[", "]", "@", "!", "$", "&", "(", ")", "*", "+", ",", ";", "=", "'"]
+
 
 import UIKit
 import Foundation
@@ -11,9 +14,6 @@ import CoreData
 
 @objcMembers
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
-//    ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:/?#[]@!$&'()*+,;=
-
-    var allowedSymbols = [ "A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", ".", "_", "~", ":", "/", "?", "#", "[", "]", "@", "!", "$", "&", "(", ")", "*", "+", ",", ";", "=", "'"]
 
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
@@ -46,26 +46,107 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+//        startButton.isEnabled = false
+        
         startUrlTextField.delegate = self
         threadCountTextField.delegate = self
         textResultTextField.delegate = self
         maxUrlCoutTextField.delegate = self
+        
+        startUrlTextField.text = "https://stackoverflow.com/"
+        threadCountTextField.text = "5"
+        textResultTextField.text = "dog"
+        maxUrlCoutTextField.text = "25"
         
 //        tableView.register(ResultTableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
 
         tableView.delegate = self
         tableView.dataSource = self
         
-        getDataFromUrl()
-        findUrlInString()
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayLinks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! ResultTableViewCell
+        
+        cell.urlLabel.text = self.arrayLinks[indexPath.row].absoluteString
+        cell.statusLabel.text = "OK"
+        
+        return cell
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == startUrlTextField {
+            var newTextUrl = textField.text! as NSString
+            newTextUrl = newTextUrl.replacingCharacters(in: range, with: string) as NSString
+//            startUrl = URL(string: newTextUrl as String)
+            print(newTextUrl)
+        } else if textField == threadCountTextField {
+            var threads = textField.text! as NSString
+            threads = threads.replacingCharacters(in: range, with: string) as NSString
+            print(threads)
+        } else if textField == textResultTextField {
+            var findText = textField.text! as NSString
+            findText = findText.replacingCharacters(in: range, with: string) as NSString
+            print(findText)
+        } else if textField == maxUrlCoutTextField {
+            var urlCount = textField.text! as NSString
+            urlCount = urlCount.replacingCharacters(in: range, with: string) as NSString
+            print(urlCount)
+        }
+        
+        if startUrlTextField != nil && threadCountTextField != nil && textResultTextField != nil && maxUrlCoutTextField != nil {
+            startButton.isEnabled = true
+        }
+
+        return true
+    }
+    
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == startUrlTextField {
+            startUrl = URL(string: textField.text!)
+            print("this is a start url " + "\(startUrl as Any)")
+        } else if textField == threadCountTextField {
+            threadCount = Int(textField.text!)
+            print("this is how many threads can be " + "\(threadCount as Any)")
+        } else if textField == textResultTextField {
+            textResult = String(textField.text!)
+            print("this is text to find " + "\(textResult as Any)")
+        } else if textField == maxUrlCoutTextField {
+            maxUrlCount = Int(textField.text!)
+            print("this is url count when we can stop " + "\(maxUrlCount as Any)")
+        }
+        
+        if startUrlTextField != nil && threadCountTextField != nil && textResultTextField != nil && maxUrlCoutTextField != nil {
+            startButton.isEnabled = true
+        }
+
     }
     
     func getDataFromUrl() {
-        let myURLString = "https://cinemaciti.ua/ua/cinema-city-kyiv/mv/bond_25/"
+       
+        guard  let myURLString = startUrlTextField.text else {
+            print("text field is empty")
+            return
+        }
+        
         guard let myURL = URL(string: myURLString) else {
             print("Error: \(myURLString) doesn't seem to be a valid URL")
             return
         }
+        urlSet.insert(myURL)
+        
 
         do {
             let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
@@ -90,86 +171,50 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 arrayLinks.append(urlCheck)
             }
             
+            baseUrl = arrayLinks.first
+            arrayLinks.removeFirst()
             print("url will be on next row")
             print(url)
         }
         print(arrayLinks)
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.arrayLinks.count
-    }
+    var urlSet: Set<URL> = []
+    var mySet = Set<String>()
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! ResultTableViewCell
-        
-        cell.urlLabel.text = self.arrayLinks[indexPath.row].absoluteString
-        cell.statusLabel.text = "OK"
-        
-        return cell
-        
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        
-        
-        print(#function)
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        if textField == startUrlTextField {
-            var newTextUrl = textField.text! as NSString
-            newTextUrl = newTextUrl.replacingCharacters(in: range, with: string) as NSString
-//            startUrl = URL(string: newTextUrl as String)
-            print(newTextUrl)
-        } else if textField == threadCountTextField {
-            var threads = textField.text! as NSString
-            threads = threads.replacingCharacters(in: range, with: string) as NSString
-            print(threads)
-        } else if textField == textResultTextField {
-            var findText = textField.text! as NSString
-            findText = findText.replacingCharacters(in: range, with: string) as NSString
-            print(findText)
-        } else if textField == maxUrlCoutTextField {
-            var urlCount = textField.text! as NSString
-            urlCount = urlCount.replacingCharacters(in: range, with: string) as NSString
-            print(urlCount)
-        }
+    var urlArrayQueue: [String] = []
 
-        return true
-    }
-    
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField == startUrlTextField {
-            startUrl = URL(string: textField.text!)
-            print("this is a start url " + "\(startUrl as Any)")
-        } else if textField == threadCountTextField {
-            threadCount = Int(textField.text!)
-            print("this is how many threads can be " + "\(threadCount as Any)")
-        } else if textField == textResultTextField {
-            textResult = String(textField.text!)
-            print("this is text to find " + "\(textResult as Any)")
-        } else if textField == maxUrlCoutTextField {
-            maxUrlCount = Int(textField.text!)
-            print("this is url count when we can stop " + "\(maxUrlCount as Any)")
-        }
+    func checkIfDataIsInSet() {
         
     }
 
-    func startButtonTapped() {
+    @IBAction func startButtonTapped(_ sender: Any) {
         startButton.isEnabled = false
+        stopButton.isEnabled = true
+        pauseButton.isEnabled = true
         print(#function)
+        
+        getDataFromUrl()
+        findUrlInString()
+        
+        urlSet.insert(startUrl)
+
+        
+        
+        tableView.reloadData()
+        
     }
     
-    func stopButtonTapped() {
+    @IBAction func stopButtonTapped(_ sender: Any) {
         stopButton.isEnabled = false
+        startButton.isEnabled = true
         print(#function)
     }
     
-    func pauseButtonTapped() {
+    @IBAction func pauseButtonTapped(_ sender: Any) {
         pauseButton.isEnabled = false
+        startButton.isEnabled = true
+        stopButton.isEnabled = true
         print(#function)
     }
 
